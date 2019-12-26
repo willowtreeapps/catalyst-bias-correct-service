23ba494c944de32d568168eecc56f370b767f965
Grayson Hansard <grayson.hansard@willowtreeapps.com>
2019-12-26 10:36:30 -0500

    Add CSV Parser that expects simple trigger/correction pairs

---
+++ test/util/CSVCorrectionsParserTests.java
@@ -0,0 +1,25 @@
+package util;
+
+import org.junit.Test;
+import play.test.WithApplication;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CSVCorrectionsParserTests extends WithApplication {
+    @Test
+    public void TestParsingCorrections() throws MalformedURLException {
+        var parser = new CSVCorrectionsParser();
+        var corrections = parser.parse(new File("test/util/CSVParsingTest.txt").toURI().toURL());
+        assertNotNull(corrections);
+        assertEquals("key1", corrections.get(0).getTrigger());
+        assertEquals("value1", corrections.get(0).getSuggestion());
+        assertEquals("key2", corrections.get(1).getTrigger());
+        assertEquals("value2", corrections.get(1).getSuggestion());
+        assertEquals("key1", corrections.get(2).getTrigger());
+        assertEquals("value3", corrections.get(2).getSuggestion());
+    }
+}
