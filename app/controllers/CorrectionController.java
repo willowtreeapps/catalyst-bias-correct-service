84ccef452789b306d8b8158d7540ac21a887b13b
Grayson Hansard <grayson.hansard@willowtreeapps.com>
2019-12-27 12:25:20 -0500

    Detect user input language

--- app/controllers/CorrectionController.java
+++ app/controllers/CorrectionController.java
@@ -5,22 +5,30 @@ import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import util.BiasCorrector;
-import util.BiasDetector;
+import util.LanguageDetectionConfidence;
+import util.LanguageDetector;
 
 import javax.inject.Inject;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Map;
 
 public class CorrectionController extends Controller {
     @Inject
-    public CorrectionController(BiasCorrector corrector) {
+    public CorrectionController(BiasCorrector corrector, LanguageDetector languageDetector) {
         _biasCorrector = corrector;
+        _languageDetector = languageDetector;
     }
 
     public Result correct(Http.Request request) {
         var textToCorrect = getString(request, TextFieldName);
         var context = getString(request, ContextFieldName);
-        var correction = _biasCorrector.correct(textToCorrect, null);
+        var languageDetection = _languageDetector.detect(textToCorrect);
+        var locale = languageDetection.getConfidence() == LanguageDetectionConfidence.HIGH
+                ? new Locale(languageDetection.getLanguage())
+                : null;
+
+        var correction = _biasCorrector.correct(textToCorrect, locale);
 
         var response = Map.of(
             "input", textToCorrect,
@@ -44,6 +52,7 @@ public class CorrectionController extends Controller {
     }
 
     private BiasCorrector _biasCorrector;
+    private LanguageDetector _languageDetector;
 
     private static String TextFieldName = "text";
     private static String ContextFieldName = "context";
