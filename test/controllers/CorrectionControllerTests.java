58931dc190cc90e6597681b41372f39cf09a3165
Grayson Hansard <grayson.hansard@willowtreeapps.com>
2019-12-31 09:01:31 -0500

    Add additional tests for testing `/correct` endpoint

--- test/controllers/CorrectionControllerTests.java
+++ test/controllers/CorrectionControllerTests.java
@@ -45,4 +45,54 @@ public class CorrectionControllerTests extends WithApplication {
         assertEquals("context-value", response.context);
     }
 
+    @Test
+    public void testCorrectionWithoutContext() {
+        var json = Json.toJson(Map.of(
+                "text", "she is keyone"
+        ));
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(POST)
+                .uri("/correct")
+                .bodyJson(json);
+
+        Result result = route(app, request);
+        var body = contentAsBytes(result).toArray();
+        var response = Json.fromJson(Json.parse(body), CorrectionController.CorrectResponse.class);
+        assertEquals(OK, result.status());
+        assertEquals("she is value1", response.correction);
+        assertEquals("she is keyone", response.input);
+        assertEquals("", response.context);
+    }
+
+    @Test
+    public void testCorrectionWithEmptyBody() {
+        var json = Json.toJson(Map.of());
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(POST)
+                .uri("/correct")
+                .bodyJson(json);
+
+        Result result = route(app, request);
+        var body = contentAsBytes(result).toArray();
+        var response = Json.fromJson(Json.parse(body), CorrectionController.CorrectResponse.class);
+        assertEquals(OK, result.status());
+        assertEquals("", response.correction);
+        assertEquals("", response.input);
+        assertEquals("", response.context);
+    }
+
+    @Test
+    public void testCorrectionWithNoBody() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(POST)
+                .uri("/correct");
+
+        Result result = route(app, request);
+        var body = contentAsBytes(result).toArray();
+        var response = Json.fromJson(Json.parse(body), CorrectionController.CorrectResponse.class);
+        assertEquals(OK, result.status());
+        assertEquals("", response.correction);
+        assertEquals("", response.input);
+        assertEquals("", response.context);
+    }
 }
